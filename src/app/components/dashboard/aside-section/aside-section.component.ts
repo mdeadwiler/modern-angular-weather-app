2d027232a557a6c81e79e45d49f7689e4854b274
Oden-student-md1 <133975362+mdeadwiler@users.noreply.github.com>
2024-03-15 22:11:35 -0400

    added components

---
+++ src/app/components/dashboard/aside-section/aside-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aside-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './aside-section.component.html',
+  styleUrl: './aside-section.component.css'
+})
+export class AsideSectionComponent {
+
+}
---
+++ src/app/components/dashboard/middle-section/middle-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-middle-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './middle-section.component.html',
+  styleUrl: './middle-section.component.css'
+})
+export class MiddleSectionComponent {
+
+}
