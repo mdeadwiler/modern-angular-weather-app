6d5ea47c57a824922d300c70196733d05a09dc26
Oden-student-md1 <133975362+mdeadwiler@users.noreply.github.com>
2024-04-07 21:47:59 -0400

    add feature/login component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatGridListModule} from '@angular/material/grid-list';
 
+/**
+ * @title Basic grid-list
+ */
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: 'grid-list-overview-example',
+  styleUrl: 'grid-list-overview-example.css',
+  templateUrl: 'grid-list-overview-example.html',
+  standalone: true,
+  imports: [MatGridListModule],
 })
+export class GridListOverviewExample {}
+
 export class LoginComponent implements OnInit {
 
   constructor() { }
